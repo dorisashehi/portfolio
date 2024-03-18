20eecdb6c4a71e8c6cab706b9d42d5ab0698a40d
Dorisa Shehi <doris.shehi2@gmail.com>
2024-03-18 10:21:49 -0500

    Projects added

--- src/projects.js
+++ src/projects.js
@@ -4,20 +4,110 @@ const projects = [
         id:1,
         img: "stanley-project.png",
         title:"Stanley",
-        description:"e-Commerce Website",
-        technologies:['Laravel','PHP','JavaScript'],
+        description:"Developed from the beginning, a Stanley e-commerce\
+        website for a French supermarket. It enables clients to purchase\
+        products at Stanley Supermarket using points accumulated from previous\
+        purchases at the supermarket.",
+        technologies:['Laravel','PHP','JQuery'],
         link: "https://stanley-booking.crispybacon.dev/"
 
     },
     {
         id:2,
-        img: "react-project-crud.png",
-        title:"Contact Manager",
-        description:"Create, Edit, Delete and Search Managers",
-        technologies:['ReatJs']
+        img: "assumed.png",
+        title:"Assumed",
+        description:"Created admin pages for the Assumed website.\
+        Assumed ensures that your partners handle their data as claimed,\
+        validates your lead/sales processes, and monitors your databases\
+        for unauthorized access or theft.",
+        technologies:['Laravel','PHP','VueJs'],
+        link: "https://app.assumed.com/login"
 
     },
-    
+    {
+        id:3,
+        img: "citylight.png",
+        title:"CityLight",
+        description:"Worked on maintaining the PHP version of the CityLight\
+        website and migrated it to the Laravel framework. This platform \
+        offers an integrated solution, allowing the public lighting network\
+        to be easily queried at any time and according to specific needs.",
+        technologies:['Laravel','PHP','JavaScript'],
+        link: "http://citylight.apps.sis-ter.it/"
+
+    },
+    {
+        id:4,
+        img: "react-project-crud.png",
+        title:"Contact Manager",
+        description:"Perform CRUD operations to create, update, delete, and read managers.",
+        technologies:['ReatJs'],
+        link: "https://github.com/dorisashehi/contact-manager"
+
+    },
+    {
+        id:5,
+        img: "react-project-crud.png",
+        title:"Portfolio",
+        description:"Created a portfolio page to showcase my work.",
+        technologies:['ReatJs'],
+        link: "https://effortless-fox-48c033.netlify.app/"
+
+    },
+    {
+        id:6,
+        img: "e-commerce.png",
+        title:"e-Commerce",
+        description:"Created the frontend of an e-commerce website.",
+        technologies:['VueJs'],
+        link: "https://github.com/dorisashehi/vuejs_ecommerce"
+
+    },
+    {
+        id:7,
+        img: "brickup.png",
+        title:"Brickup",
+        description:"Maintained the Brick Up website, developed using\
+        Node.js, MongoDB, Mongoose, and Keystone.js. BrickUp is an\
+        innovative web platform authorized by CONSOB, bringing together\
+        virtuous construction entrepreneurs and investors eager to diversify\
+        capital allocation in high-value real estate projects through\
+        real estate equity crowdfunding.",
+        technologies:['NodeJs','Mongoose','MongoDB', "KeystoneJs"],
+        link: "https://brickup.it/"
+
+    },
+    {
+        id:8,
+        img: "capellimonelli.png",
+        title:"Capellimonelli",
+        description:"Created the frontend of the Capellimonelli website\
+        using Magezon Page Builder. It is an online seller and retailer of hair care products.",
+        technologies:['Magento 2','PHP','JQuery'],
+        link: "https://capellimonelli.it/"
+
+    },
+    {
+        id:9,
+        img: "kegland.png",
+        title:"Kegland",
+        description:"Upgraded the Kegland e-commerce website from Magento 1 to Magento 2 version. \
+        Kegland is an e-commerce platform specializing in selling brewing supplies for every brewer.",
+        technologies:['Magento 2','PHP','JQuery'],
+        link: "https://www.kegland.com.au/"
+
+    },
+    {
+        id:10,
+        img: "seitron.png",
+        title:"Seitron",
+        description:"Maintained an existing Magento 2 websites Seitron integrated with WordPress. \
+        Seitron is an italian company leader in thermoregulation, residential and\
+        industrial gas detectors, combustions and emissions analyzers.",
+        technologies:['Magento 2','PHP','JQuery'],
+        link: "https://seitron.com/it/"
+
+    },
 ]
 
 export default projects;
