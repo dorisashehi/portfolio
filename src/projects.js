c4efb846e8fec9ca80c49b4725117ad909061dba
Dorisa Shehi <doris.shehi2@gmail.com>
2024-03-18 10:28:07 -0500

    fixed problems

--- src/projects.js
+++ src/projects.js
@@ -4,10 +4,7 @@ const projects = [
         id:1,
         img: "stanley-project.png",
         title:"Stanley",
-        description:"Developed from the beginning, a Stanley e-commerce\
-        website for a French supermarket. It enables clients to purchase\
-        products at Stanley Supermarket using points accumulated from previous\
-        purchases at the supermarket.",
+        description:"Developed from the beginning, a Stanley e-commerce website for a French supermarket. It enables clients to purchase products at Stanley Supermarket using points accumulated from previous purchases at the supermarket.",
         technologies:['Laravel','PHP','JQuery'],
         link: "https://stanley-booking.crispybacon.dev/"
 
@@ -16,10 +13,7 @@ const projects = [
         id:2,
         img: "assumed.png",
         title:"Assumed",
-        description:"Created admin pages for the Assumed website.\
-        Assumed ensures that your partners handle their data as claimed,\
-        validates your lead/sales processes, and monitors your databases\
-        for unauthorized access or theft.",
+        description:"Created admin pages for the Assumed website. Assumed ensures that your partners handle their data as claimed, validates your lead/sales processes, and monitors your databases for unauthorized access or theft.",
         technologies:['Laravel','PHP','VueJs'],
         link: "https://app.assumed.com/login"
 
@@ -28,10 +22,7 @@ const projects = [
         id:3,
         img: "citylight.png",
         title:"CityLight",
-        description:"Worked on maintaining the PHP version of the CityLight\
-        website and migrated it to the Laravel framework. This platform \
-        offers an integrated solution, allowing the public lighting network\
-        to be easily queried at any time and according to specific needs.",
+        description:"Worked on maintaining the PHP version of the CityLight website and migrated it to the Laravel framework. This platform offers an integrated solution, allowing the public lighting network to be easily queried at any time and according to specific needs.",
         technologies:['Laravel','PHP','JavaScript'],
         link: "http://citylight.apps.sis-ter.it/"
 
@@ -67,12 +58,7 @@ const projects = [
         id:7,
         img: "brickup.png",
         title:"Brickup",
-        description:"Maintained the Brick Up website, developed using\
-        Node.js, MongoDB, Mongoose, and Keystone.js. BrickUp is an\
-        innovative web platform authorized by CONSOB, bringing together\
-        virtuous construction entrepreneurs and investors eager to diversify\
-        capital allocation in high-value real estate projects through\
-        real estate equity crowdfunding.",
+        description:"Maintained the Brick Up website, developed using Node.js, MongoDB, Mongoose, and Keystone.js. BrickUp is an innovative web platform authorized by CONSOB, bringing together virtuous construction entrepreneurs and investors eager to diversify capital allocation in high-value real estate projects through real estate equity crowdfunding.",
         technologies:['NodeJs','Mongoose','MongoDB', "KeystoneJs"],
         link: "https://brickup.it/"
 
@@ -81,8 +67,7 @@ const projects = [
         id:8,
         img: "capellimonelli.png",
         title:"Capellimonelli",
-        description:"Created the frontend of the Capellimonelli website\
-        using Magezon Page Builder. It is an online seller and retailer of hair care products.",
+        description:"Created the frontend of the Capellimonelli website using Magezon Page Builder. It is an online seller and retailer of hair care products.",
         technologies:['Magento 2','PHP','JQuery'],
         link: "https://capellimonelli.it/"
 
@@ -91,8 +76,7 @@ const projects = [
         id:9,
         img: "kegland.png",
         title:"Kegland",
-        description:"Upgraded the Kegland e-commerce website from Magento 1 to Magento 2 version. \
-        Kegland is an e-commerce platform specializing in selling brewing supplies for every brewer.",
+        description:"Upgraded the Kegland e-commerce website from Magento 1 to Magento 2 version. Kegland is an e-commerce platform specializing in selling brewing supplies for every brewer.",
         technologies:['Magento 2','PHP','JQuery'],
         link: "https://www.kegland.com.au/"
 
@@ -101,9 +85,7 @@ const projects = [
         id:10,
         img: "seitron.png",
         title:"Seitron",
-        description:"Maintained an existing Magento 2 websites Seitron integrated with WordPress. \
-        Seitron is an italian company leader in thermoregulation, residential and\
-        industrial gas detectors, combustions and emissions analyzers.",
+        description:"Maintained an existing Magento 2 websites Seitron integrated with WordPress. Seitron is an italian company leader in thermoregulation, residential and industrial gas detectors, combustions and emissions analyzers.",
         technologies:['Magento 2','PHP','JQuery'],
         link: "https://seitron.com/it/"
 
