25b57ebb89d3f43e97c1d71ed00d1e2cc2f2565d
Dorisa Shehi <doris.shehi2@gmail.com>
2024-06-25 09:40:01 +0200

    New Project added to the liat of projects

--- src/projects.js
+++ src/projects.js
@@ -1,153 +1,165 @@
 const projects = [
+  {
+    id: 1,
+    img: 'stanley-project.png',
+    title: 'Stanley',
+    description:
+      'Developed from the beginning, a Stanley e-commerce website for a French supermarket. It enables clients to purchase products at Stanley Supermarket using points accumulated from previous purchases at the supermarket.',
+    technologies: ['Laravel', 'PHP', 'JQuery'],
+    link: 'https://stanley-booking.crispybacon.dev/',
+  },
+  {
+    id: 2,
+    img: 'assumed.png',
+    title: 'Assumed',
+    description:
+      'Created admin pages for the Assumed website. Assumed ensures that your partners handle their data as claimed, validates your lead/sales processes, and monitors your databases for unauthorized access or theft.',
+    technologies: ['Laravel', 'PHP', 'VueJs'],
+    link: 'https://app.assumed.com/login',
+  },
 
-    {
-        id:1,
-        img: "stanley-project.png",
-        title:"Stanley",
-        description:"Developed from the beginning, a Stanley e-commerce website for a French supermarket. It enables clients to purchase products at Stanley Supermarket using points accumulated from previous purchases at the supermarket.",
-        technologies:['Laravel','PHP','JQuery'],
-        link: "https://stanley-booking.crispybacon.dev/"
+  {
+    id: 3,
+    img: 'citylight.png',
+    title: 'CityLight',
+    description:
+      'Worked on maintaining the PHP version of the CityLight website and migrated it to the Laravel framework. This platform offers an integrated solution, allowing the public lighting network to be easily queried at any time and according to specific needs.',
+    technologies: ['Laravel', 'PHP', 'JavaScript'],
+    link: 'http://citylight.apps.sis-ter.it/',
+  },
+  {
+    id: 3,
+    img: 'cv.png',
+    title: 'CV Maker',
+    description: 'Created a CV maker website to create and download it.',
+    technologies: ['React.js', 'HTML', 'Sass'],
+    link: 'https://resumemaker24.netlify.app/',
+  },
 
-    },
-    {
-        id:2,
-        img: "assumed.png",
-        title:"Assumed",
-        description:"Created admin pages for the Assumed website. Assumed ensures that your partners handle their data as claimed, validates your lead/sales processes, and monitors your databases for unauthorized access or theft.",
-        technologies:['Laravel','PHP','VueJs'],
-        link: "https://app.assumed.com/login"
+  {
+    id: 3,
+    img: 'weather.png',
+    title: 'Weather App',
+    description:
+      'Search locations to show the weather data and switch temperature form (F/C)',
+    technologies: [
+      'JavaScript',
+      'HTML',
+      'Sass',
+      'ES6',
+      'Async/Await',
+      'Promisses',
+    ],
+    link: 'https://dorisashehi.github.io/weather-app/',
+  },
+  {
+    id: 3,
+    img: 'todo.png',
+    title: 'ToDo list',
+    description:
+      "That's a website to create projects and add their corresponding tasks. User can add, remove and mark as completed their tasks",
+    technologies: ['JavaScript', 'HTML', 'Sass', 'ES6', 'localStorage'],
+    link: 'https://dorisashehi.github.io/ToDo-List/',
+  },
 
-    },
+  {
+    id: 3,
+    img: 'restaurant.png',
+    title: 'Restaurant',
+    description:
+      "That's a website to show reataurant menu, location, and make a reservation. Only frontend built",
+    technologies: ['JavaScript', 'HTML', 'Sass', 'ES6'],
+    link: 'https://dorisashehi.github.io/restaurant/',
+  },
 
-    {
-        id:3,
-        img: "citylight.png",
-        title:"CityLight",
-        description:"Worked on maintaining the PHP version of the CityLight website and migrated it to the Laravel framework. This platform offers an integrated solution, allowing the public lighting network to be easily queried at any time and according to specific needs.",
-        technologies:['Laravel','PHP','JavaScript'],
-        link: "http://citylight.apps.sis-ter.it/"
+  {
+    id: 4,
+    img: 'seitron.png',
+    title: 'Seitron',
+    description:
+      'Created from the beginning, using Gutenberg Blocks, some blog pages.',
+    technologies: ['WordPress', 'PHP', 'JavaScript'],
+    link: 'https://seitron.com/it/',
+  },
+  {
+    id: 5,
+    img: 'peterjungblut.png',
+    title: 'Peterjungblut',
+    description:
+      'Made a plugin to integrate WooCommerce subscriptions to BuddyPress profile.',
+    technologies: ['WordPress', 'PHP', 'JavaScript'],
+    link: 'https://peterjungblut.de/',
+  },
+  {
+    id: 6,
+    img: 'clusteragrifood.png',
+    title: 'Clusteragrifood',
+    description:
+      'Maintained frontend of the website using Elementor, WordPress and created a plugin from beggining.',
+    technologies: ['WordPress', 'PHP', 'JavaScript'],
+    link: 'https://clusteragrifood.it/',
+  },
+  {
+    id: 7,
+    img: 'react-project-crud.png',
+    title: 'Contact Manager',
+    description:
+      'Perform CRUD operations to create, update, delete, and read managers.',
+    technologies: ['ReatJs'],
+    link: 'https://github.com/dorisashehi/contact-manager',
+  },
+  {
+    id: 8,
+    img: 'portfolio.png',
+    title: 'Portfolio',
+    description: 'Created a portfolio page to showcase my work.',
+    technologies: ['ReatJs'],
+    link: 'https://effortless-fox-48c033.netlify.app/',
+  },
+  {
+    id: 9,
+    img: 'e-commerce.png',
+    title: 'e-Commerce',
+    description: 'Created the frontend of an e-commerce website.',
+    technologies: ['VueJs'],
+    link: 'https://github.com/dorisashehi/vuejs_ecommerce',
+  },
+  {
+    id: 10,
+    img: 'brickup.png',
+    title: 'Brickup',
+    description:
+      'Maintained the Brick Up website, developed using Node.js, MongoDB, Mongoose, and Keystone.js. BrickUp is an innovative web platform authorized by CONSOB, bringing together virtuous construction entrepreneurs and investors eager to diversify capital allocation in high-value real estate projects through real estate equity crowdfunding.',
+    technologies: ['NodeJs', 'Mongoose', 'MongoDB', 'KeystoneJs'],
+    link: 'https://brickup.it/',
+  },
+  {
+    id: 11,
+    img: 'capellimonelli.png',
+    title: 'Capellimonelli',
+    description:
+      'Created the frontend of the Capellimonelli website using Magezon Page Builder. It is an online seller and retailer of hair care products.',
+    technologies: ['Magento 2', 'PHP', 'JQuery'],
+    link: 'https://capellimonelli.it/',
+  },
+  // {
+  //     id:12,
+  //     img: "kegland.png",
+  //     title:"Kegland",
+  //     description:"Upgraded the Kegland e-commerce website from Magento 1 to Magento 2 version. Kegland is an e-commerce platform specializing in selling brewing supplies for every brewer.",
+  //     technologies:['Magento 2','PHP','JQuery'],
+  //     link: "https://www.kegland.com.au/"
 
-    },
+  // },
+  // {
+  //     id:13,
+  //     img: "seitron.png",
+  //     title:"Seitron",
+  //     description:"Maintained an existing Magento 2 websites Seitron integrated with WordPress. Seitron is an italian company leader in thermoregulation, residential and industrial gas detectors, combustions and emissions analyzers.",
+  //     technologies:['Magento 2','PHP','JQuery'],
+  //     link: "https://seitron.com/it/"
 
-    {
-        id:3,
-        img: "weather.png",
-        title:"Weather App",
-        description:"Search locations to show the weather data and switch temperature form (F/C)",
-        technologies:['JavaScript','HTML', 'Sass', 'ES6', 'Async/Await', 'Promisses'],
-        link: "https://dorisashehi.github.io/weather-app/"
-
-    },
-    {
-        id:3,
-        img: "todo.png",
-        title:"ToDo list",
-        description:"That's a website to create projects and add their corresponding tasks. User can add, remove and mark as completed their tasks",
-        technologies:['JavaScript','HTML','Sass', "ES6", "localStorage"],
-        link: "https://dorisashehi.github.io/ToDo-List/"
-
-    },
-
-    {
-        id:3,
-        img: "restaurant.png",
-        title:"Restaurant",
-        description:"That's a website to show reataurant menu, location, and make a reservation. Only frontend built",
-        technologies:['JavaScript','HTML','Sass', "ES6"],
-        link: "https://dorisashehi.github.io/restaurant/"
-
-    },
-
-    {
-        id:4,
-        img: "seitron.png",
-        title:"Seitron",
-        description:"Created from the beginning, using Gutenberg Blocks, some blog pages.",
-        technologies:['WordPress','PHP','JavaScript'],
-        link: "https://seitron.com/it/"
-
-    },
-    {
-        id:5,
-        img: "peterjungblut.png",
-        title:"Peterjungblut",
-        description:"Made a plugin to integrate WooCommerce subscriptions to BuddyPress profile.",
-        technologies:['WordPress','PHP','JavaScript'],
-        link: "https://peterjungblut.de/"
-
-    },
-    {
-        id:6,
-        img: "clusteragrifood.png",
-        title:"Clusteragrifood",
-        description:"Maintained frontend of the website using Elementor, WordPress and created a plugin from beggining.",
-        technologies:['WordPress','PHP','JavaScript'],
-        link: "https://clusteragrifood.it/"
-
-    },
-    {
-        id:7,
-        img: "react-project-crud.png",
-        title:"Contact Manager",
-        description:"Perform CRUD operations to create, update, delete, and read managers.",
-        technologies:['ReatJs'],
-        link: "https://github.com/dorisashehi/contact-manager"
-
-    },
-    {
-        id:8,
-        img: "portfolio.png",
-        title:"Portfolio",
-        description:"Created a portfolio page to showcase my work.",
-        technologies:['ReatJs'],
-        link: "https://effortless-fox-48c033.netlify.app/"
-
-    },
-    {
-        id:9,
-        img: "e-commerce.png",
-        title:"e-Commerce",
-        description:"Created the frontend of an e-commerce website.",
-        technologies:['VueJs'],
-        link: "https://github.com/dorisashehi/vuejs_ecommerce"
-
-    },
-    {
-        id:10,
-        img: "brickup.png",
-        title:"Brickup",
-        description:"Maintained the Brick Up website, developed using Node.js, MongoDB, Mongoose, and Keystone.js. BrickUp is an innovative web platform authorized by CONSOB, bringing together virtuous construction entrepreneurs and investors eager to diversify capital allocation in high-value real estate projects through real estate equity crowdfunding.",
-        technologies:['NodeJs','Mongoose','MongoDB', "KeystoneJs"],
-        link: "https://brickup.it/"
-
-    },
-    {
-        id:11,
-        img: "capellimonelli.png",
-        title:"Capellimonelli",
-        description:"Created the frontend of the Capellimonelli website using Magezon Page Builder. It is an online seller and retailer of hair care products.",
-        technologies:['Magento 2','PHP','JQuery'],
-        link: "https://capellimonelli.it/"
-
-    },
-    // {
-    //     id:12,
-    //     img: "kegland.png",
-    //     title:"Kegland",
-    //     description:"Upgraded the Kegland e-commerce website from Magento 1 to Magento 2 version. Kegland is an e-commerce platform specializing in selling brewing supplies for every brewer.",
-    //     technologies:['Magento 2','PHP','JQuery'],
-    //     link: "https://www.kegland.com.au/"
-
-    // },
-    // {
-    //     id:13,
-    //     img: "seitron.png",
-    //     title:"Seitron",
-    //     description:"Maintained an existing Magento 2 websites Seitron integrated with WordPress. Seitron is an italian company leader in thermoregulation, residential and industrial gas detectors, combustions and emissions analyzers.",
-    //     technologies:['Magento 2','PHP','JQuery'],
-    //     link: "https://seitron.com/it/"
-
-    // },
+  // },
 ]
 
-export default projects;
+export default projects
