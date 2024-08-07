a3134c0d7b85aa740185e41c4a05fe5e68d7213f
Dorisa Shehi <doris.shehi2@gmail.com>
2024-08-07 11:23:12 +0200

    Website made responsive

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,40 +1,76 @@
-import { Link } from 'react-router-dom';
-import AnimatedLetters from'../AnimatedLetters'
-import './index.scss';
+import { Link } from 'react-router-dom'
+import AnimatedLetters from '../AnimatedLetters'
+import './index.scss'
 import ProfileImg from '../../assets/images/profile.png'
-import {useState } from 'react';
+import { useState } from 'react'
 
 const Home = () => {
+  const [letterClass] = useState('text-animate')
+  const nameArray = [
+    ' ',
+    'D',
+    'o',
+    'r',
+    'i',
+    's',
+    'a',
+    ' ',
+    'S',
+    'h',
+    'e',
+    'h',
+    'i',
+  ]
+  const jobArray = [
+    'W',
+    'e',
+    'b',
+    ' ',
+    'D',
+    'e',
+    'v',
+    'e',
+    'l',
+    'o',
+    'p',
+    'e',
+    'r',
+    '.',
+  ]
 
-    const [letterClass] = useState('text-animate');
-    const nameArray = [' ','D','o','r','i','s','a',' ','S','h','e','h','i'];
-    const jobArray = ['W','e','b',' ','D','e','v','e','l','o','p','e','r','.'];
+  return (
+    <div className="container home-page">
+      <div className="text-zone">
+        <h1>
+          <span className={letterClass}>H</span>
+          <span className={`${letterClass} _12`}>i,</span>
+          <br />
+          <span className={`${letterClass} _13`}>I </span>
+          <span className={`${letterClass} _14`}>'m</span>
 
-    return(
-        <div className="container home-page">
-            <div className="text-zone">
-                <h1>
-                    <span className={letterClass}>H</span>
-                    <span className={`${letterClass} _12`}>i,</span>
-                    <br />
-                    <span className={`${letterClass} _13`}>I </span>
-                    <span className={`${letterClass} _14`}>'m</span>
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={nameArray}
+            idx={15}
+          />
+          <br />
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={jobArray}
+            idx={22}
+          />
+        </h1>
+        <h2>Full-stack Developer </h2>
+        <Link to="/contact" className="flat-button">
+          CONTACT ME
+        </Link>
+      </div>
 
-                    <AnimatedLetters  letterClass = {letterClass} strArray = {nameArray} idx = {15}/>
-                    <br/>
-                    <AnimatedLetters  letterClass = {letterClass} strArray = {jobArray} idx = {22}/>
-                </h1>
-                <h2>Full-stack Developer </h2>
-                <Link to="/contact" className='flat-button'>CONTACT ME</Link>
-            </div>
-
-            <div className='profile-picture'>
+      {/* <div className='profile-picture'>
                 <img className='sub-logo' src={ProfileImg} alt="Dorisa"/>
-            </div>
-
-
-        </div>
-    )
+            </div> */}
+    </div>
+  )
 }
 
-export default Home;
+export default Home
--- src/Components/Projects/index.js
+++ src/Components/Projects/index.js
@@ -1,32 +1,29 @@
 import './index.scss'
-import AnimatedLetters from "../AnimatedLetters";
-import { useState } from 'react';
+import AnimatedLetters from '../AnimatedLetters'
+import { useState } from 'react'
 import projects from '../../projects'
-import Project from './Project';
+import Project from './Project'
 
 const Projects = () => {
+  const [letterClass] = useState('text-animate')
 
-    const [letterClass] = useState('text-animate');
-
-    return (
-        <div className="container projects-page">
-            <div className="text-zone">
-                <h1>
+  return (
+    <div className="container projects-page">
+      <div className="text-zone">
+        {/* <h1>
                     <AnimatedLetters  letterClass = {letterClass}
                         strArray={['M','y',' ','P','r','o','j','e', 'c', 't', 's']}
                         idx={15}
                     />
-                </h1>
-                <div className='projects-container'>
-                    {
-                        projects.map((project, index)=>{
-                           return <Project key = {project.id} project = {project} />
-                        })
-                    }
-                </div>
-            </div>
+                </h1> */}
+        <div className="projects-container">
+          {projects.map((project, index) => {
+            return <Project key={project.id} project={project} />
+          })}
         </div>
-    )
+      </div>
+    </div>
+  )
 }
 
-export default Projects;
+export default Projects
