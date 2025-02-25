1595a321bd2905383a57ea110cfcd06a72fa3538
Dorisa Shehi <doris.shehi2@gmail.com>
2025-02-25 09:44:31 -0500

    Technologies changed

--- src/Components/About/index.js
+++ src/Components/About/index.js
@@ -1,32 +1,39 @@
-import './index.scss';
-import AnimatedLetters from "../AnimatedLetters";
-import { useState } from 'react';
+import './index.scss'
+import AnimatedLetters from '../AnimatedLetters'
+import { useState } from 'react'
 
 const About = () => {
+  const [letterClass] = useState('text-animate')
 
-    const [letterClass] = useState('text-animate');
+  return (
+    <div className="container about-page">
+      <div className="text-zone">
+        <h1>
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={['A', 'b', 'o', 'u', 't', ' ', 'M', 'e']}
+            idx={15}
+          />
+        </h1>
+        <p>
+          👨‍💻 As a web developer, I am actively seeking employment opportunities
+          to enhance my skills in web development and gain valuable professional
+          experience.
+        </p>
 
-    return(
-        <div className="container about-page">
-            <div className="text-zone">
-                <h1>
-                    <AnimatedLetters  letterClass = {letterClass}
-                        strArray={['A','b','o','u','t', ' ', 'M', 'e']}
-                        idx={15}
-                    />
-                </h1>
-                <p>👨‍💻 As a web developer, I am actively seeking employment opportunities to enhance my skills in
-                    web development and gain valuable professional experience.</p>
-
-                <p>💡 With several years of experience, I am proficient in PHP, Laravel, Vue.js and Javascript. Additionally, I possess hands-on experience
-                    with NodeJS, React.js, WordPress, and JQuery.</p>
-                <p>
-                    🔍 Open to new opportunities and challenges where I can apply my skills, learn from innovative projects, and contribute to the company's success.
-                </p>
-
-            </div>
-        </div>
-    )
+        <p>
+          💡 With several years of experience, I am proficient in JavaScript,
+          React.js, Node.js, Expess.js, PHP, and Laravel. Additionally, I
+          possess hands-on experience with Laravel, Vue.js, and JQuery.
+        </p>
+        <p>
+          🔍 Open to new opportunities and challenges where I can apply my
+          skills, learn from innovative projects, and contribute to the
+          company's success.
+        </p>
+      </div>
+    </div>
+  )
 }
 
-export default About;
+export default About
